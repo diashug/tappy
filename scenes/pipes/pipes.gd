549ad04ebe82b0a46affe039142b3cc83f1071f4
Hugo Dias <hmdias@gmail.com>
2025-02-10 22:54:36 +0000

    Add pipes scrolling

---
+++ scenes/pipes/pipes.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position.x -= delta * 120.0
+	check_off_screen()
+
+func check_off_screen() -> void:
+	if position.x < -250.0:
+		queue_free()
+
+func _on_screen_exited() -> void: # only works if the screen is being rendered (don't work when minimised)
+	queue_free()
