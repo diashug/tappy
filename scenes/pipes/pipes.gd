429d7807c0cce16db97f9e61bd26ac47ba5668fd
Hugo Dias <hmdias@gmail.com>
2025-03-03 22:01:16 +0000

    Add game manager autoload

--- scenes/pipes/pipes.gd
+++ scenes/pipes/pipes.gd
@@ -2,13 +2,15 @@ extends Node2D
 
 class_name Pipes
 
+const OFF_SCREEN: float = -250.0
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position.x -= delta * 120.0
+	position.x -= delta * GameManager.SCROLL_SPEED
 	check_off_screen()
 
 func check_off_screen() -> void:
-	if position.x < -250.0:
+	if position.x < OFF_SCREEN:
 		queue_free()
 
 func _on_screen_exited() -> void: # only works if the screen is being rendered (don't work when minimised)
