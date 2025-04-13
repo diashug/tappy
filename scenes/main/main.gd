f9527d3020a95b88e331d6382b3c148d4ead7acf
Hugo Dias <hmdias@gmail.com>
2025-04-13 22:32:15 +0100

    Add set score and high score

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -1,10 +1,10 @@
 extends Control
 
+@onready var highscore_label: Label = $MarginContainer/HighscoreLabel
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	highscore_label.text = str(ScoreManager.get_high_score())
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
